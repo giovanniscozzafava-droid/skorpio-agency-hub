39359f89e037106156eafd4e55d7791542027be9
giovanniscozzafava-droid <giovanni.scozzafava@gmail.com>
2026-04-12 22:17:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Index from './pages/Index';
 import { ResetPassword } from './pages/ResetPassword';
 import GCalCallback from './pages/GCalCallback';
 import GDriveCallback from './pages/GDriveCallback';
+import TvPlayer from './pages/TvPlayer';
 import NotFound from './pages/NotFound';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Index />} />
+      <Route path="/tv/:slug" element={<TvPlayer />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/gcal-callback" element={<GCalCallback />} />
       <Route path="/gdrive-callback" element={<GDriveCallback />} />
